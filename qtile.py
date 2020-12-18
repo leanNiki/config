8ff29a5fcb33d24f01373ade05853469c8c55e65
leanNiki <niklas.mirwani@lean-coders.at>
2020-12-18 17:22:11 +0100

    qtile updates

--- qtile.py
+++ qtile.py
@@ -20,64 +20,53 @@ blue = "#7cafc2"
 yellow = "#f7ca88"
 white = "#f8f8f8"
 foreground = "#282828"
+background = "#ffffff"
 
 keys = [
-    Key([m, s],  "r", lazy.restart()),
-    Key([m, s],  "q", lazy.shutdown()),
-    Key([m, s], "c", lazy.window.kill()),
-    Key([m], "space", lazy.next_layout()),
-    Key([m, s], "space", lazy.layout.reset()),
-    Key([m],  "q", lazy.to_screen(0), lazy.group.toscreen(0)),
-    Key([m],  "w", lazy.to_screen(1), lazy.group.toscreen(1)),
-    Key([m],  "e", lazy.to_screen(2), lazy.group.toscreen(2)),
 
-    # Bindings to control the layouts
-    Key([m], "t", lazy.window.toggle_floating()),
-    Key([m], "f", lazy.window.toggle_fullscreen()),
-    # These are unique to stack layout
-    Key([m], "h", lazy.layout.left()),
-    Key([m], "l", lazy.layout.right()),
-    Key([m], "j", lazy.layout.down()),
-    Key([m], "k", lazy.layout.up()),
+    Key([m, s], "Return", lazy.spawn("urxvt")),
+    Key([m, s], "c", lazy.window.kill()),
+    Key([m, s], "e", lazy.spawn("emacs")),
     Key([m, s], "h", lazy.layout.swap_left()),
-    Key([m, s], "l", lazy.layout.swap_right()),
     Key([m, s], "j", lazy.layout.shuffle_down()),
     Key([m, s], "k", lazy.layout.shuffle_up()),
-    Key([m], "i", lazy.layout.grow()),
-    Key([m], "u", lazy.layout.shrink()),
-    Key([m], "o", lazy.layout.maximize()),
-    # Launching applications
-    # Qtile application launcher
-    Key([m], "r", lazy.spawncmd(prompt='shell>')),
-    # Application Launcher
-    Key([m], "p", lazy.spawn("dmenu_run")),
-    # Terminal Application
-    Key([m, s], "Return", lazy.spawn("urxvt")),
-    # Emacs
-    Key([m, s], "e", lazy.spawn("emacs")),
-    # Browser
+    Key([m, s], "l", lazy.layout.swap_right()),
+    Key([m, s], "p", lazy.spawn("mpc toggle")),
+    Key([m, s], "q", lazy.shutdown()),
+    Key([m, s], "r", lazy.restart()),
+    Key([m, s], "space", lazy.layout.reset()),
     Key([m, s], "w", lazy.spawn("brave")),
 
-    # Change the volume and music if our keyboard has keys
-    Key( [], "XF86AudioRaiseVolume", lazy.spawn("pulsemixer --change-volume +5")),
-    Key( [], "XF86AudioLowerVolume", lazy.spawn("pulsemixer --change-volume -5")),
-    Key( [], "XF86AudioMute", lazy.spawn("pulsemixer --toggle-mute")),
-    Key( [], "XF86AudioPrev", lazy.spawn("mpc prev")),
-    Key( [], "XF86AudioNext", lazy.spawn("mpc next")),
-    Key( [], "XF86AudioPlay", lazy.spawn("mpc toggle")),
-    # Change monitor brightness if keyboard has keys
-    Key( [], "XF86MonBrightnessUp", lazy.spawn("xbacklight +5")),
-    Key( [], "XF86MonBrightnessDown", lazy.spawn("xbacklight -4")),
+    Key([m], "Up", lazy.spawn("pulsemixer --change-volume +5")),
+    Key([m], "Down", lazy.spawn("pulsemixer --change-volume -5")),
+    Key([m], "Left", lazy.screen.prev_group()),
+    Key([m], "Right", lazy.screen.next_group()),
+    Key([m], "q", lazy.to_screen(0), lazy.group.toscreen(0)),
+    Key([m], "w", lazy.to_screen(1), lazy.group.toscreen(1)),
+    Key([m], "e", lazy.to_screen(2), lazy.group.toscreen(2)),
+    Key([m], "space", lazy.next_layout()),
+    Key([m], "f", lazy.window.toggle_fullscreen()),
+    Key([m], "m", lazy.layout.maximize()),
+    Key([m], "h", lazy.layout.shrink()),
+    Key([m], "j", lazy.layout.down()),
+    Key([m], "k", lazy.layout.up()),
+    Key([m], "l", lazy.layout.grow()),
+    Key([m], "r", lazy.spawn("dmenu_run -i -nb white -nf black -sb '#A1B56C'")),
+    Key([m], "t", lazy.window.toggle_floating()),
 
-    # also allow changing volume and music the old fashioned way
-    Key( [m], "Left", lazy.spawn("pulsemixer --change-volume -5")),
-    Key( [m], "Right", lazy.spawn("pulsemixer --change-volume +5")),
-    # mpd controls
-    Key( [m], "minus", lazy.spawn("mpc volume -10")),
-    Key( [m], "plus", lazy.spawn("mpc volume +10")),
-    Key( [m], "less", lazy.spawn("mpc prev")),
-    Key( [m, s], "less", lazy.spawn("mpc next")),
-    Key( [m, s], "p", lazy.spawn("mpc toggle")),
+    Key([m], "n", lazy.spawn("mpc next")),
+    Key([m], "p", lazy.spawn("mpc prev")),
+    Key([m], "plus", lazy.spawn("mpc volume +10")),
+    Key([m], "minus", lazy.spawn("mpc volume -10")),
+
+    Key( [], "XF86AudioMute", lazy.spawn("pulsemixer --toggle-mute")),
+    Key( [], "XF86AudioNext", lazy.spawn("mpc next")),
+    Key( [], "XF86AudioPrev", lazy.spawn("mpc prev")),
+    Key( [], "XF86AudioPlay", lazy.spawn("mpc toggle")),
+    Key( [], "XF86AudioLowerVolume", lazy.spawn("pulsemixer --change-volume -5")),
+    Key( [], "XF86AudioRaiseVolume", lazy.spawn("pulsemixer --change-volume +5")),
+    Key( [], "XF86MonBrightnessDown", lazy.spawn("xbacklight -4")),
+    Key( [], "XF86MonBrightnessUp", lazy.spawn("xbacklight +5")),
 ]
 
 mouse = [
@@ -94,7 +83,7 @@ for i in groups:
     keys.append(Key([m, s], i.name, lazy.window.togroup(i.name)) )
 
 border = dict(
-    border_normal='#383838',
+    border_normal='#e8e8e8',
     border_focus='#a1b56c',
     border_width=2,
 )
@@ -126,6 +115,7 @@ widget_defaults = dict(
     font='sans',
     fontsize=14,
     padding=3,
+    foreground=foreground
 )    
 
 # Define StatusBar
@@ -133,7 +123,7 @@ def getBar():
     return bar.Bar([
         widget.GroupBox(
             margin_x = 0,
-            margin_y = 2,
+            margin_y = 3,
             borderwidth = 0,
             padding_x = 10,
             rounded = False,
@@ -142,76 +132,57 @@ def getBar():
             this_current_screen_border = "a1b56c",
             this_screen_border = "505a36",
             other_screen_border = "888888",
-            inactive = "606060",
+            inactive = "d0d0d0",
+            active = foreground,
+            background = background
         ),
         widget.Prompt(),
-        widget.WindowName(foreground = "a0a0a0",),
-        widget.Notify(),
-        widget.TextBox(
-            text = "B:",
-            foreground = foreground,
-            background = red,
-        ),
+        widget.WindowName(foreground = foreground, background = background,),
+        widget.Notify(foreground = foreground, background = background,),
         widget.Battery(
-            foreground = foreground,
-            background = red,
+            background = "#a1b56c",
             low_foreground = white,
             low_percentage = 0.15,
             update_delay = 10,
             charge_char = "+",
             discharge_char = "-",
-        ),
-        widget.TextBox(
-            text = "N:",
-            foreground = foreground,
-            background = red,
-        ),
-        widget.Wlan(
-            interface = "wlo1",
-            foreground = foreground,
-            background = red,
+            format = '{char} {percent:2.0%} {hour:d}:{min:02d}'
         ),
         widget.Net(
             interface = "wlo1",
             foreground = white,
-            background = red,
+            background = "#a1b56c",
+            format = '{down} ↓↑ {up}'
         ),
-        widget.TextBox(
-            text = "C:",
-            foreground = foreground,
+        widget.Wlan(
+            interface = "wlo1",
+            background = "#a1b56c",
+            format = '{essid} {percent:2.0%}',
+        ),
+        widget.Memory(
             background = yellow,
         ),
-        widget.CPUGraph(
-            line_width = 1,
-            foreground = foreground,
+        widget.CPU(
             background = yellow,
-        ),
-        widget.TextBox(
-            text = "M:",
-            foreground = foreground,
-            background = yellow,
-        ),
-        widget.MemoryGraph(
-            line_width = 1,
-            foreground = foreground,
-            background = yellow,
+            format = 'C: {load_percent}%'
         ),
         widget.TextBox(
             text = "V:",
-            foreground = foreground,
             background = yellow,
         ),
         widget.Volume(
-            foreground = foreground,
             background = yellow,
         ),
         widget.Clock(
             format = "%a %d %b %H:%M:%S",
-            foreground = foreground,
+            background = blue,
+        ),
+        widget.YahooWeather(
+            woeid = '551801',
+            format = '{current_observation_condition_temperature}°C',
             background = blue,
         ),
         widget.Systray(
-            foreground = foreground,
             background = blue,
         ),
     ], 20)
